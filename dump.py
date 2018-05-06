b15018d1fc75b866b5c10195fea7a1f6a999806a
Martin Gallo <martin.gallo@gmail.com>
2018-05-05 22:21:38 -0300

    Delta dump

--- dump.py
+++ dump.py
@@ -1,17 +1,35 @@
+import sys
 import json
 import requests
 import demjson
 
-tarjeta_inicio = 10000000
-tarjeta_fin    = 10000100
+
+if len(sys.argv) == 3:
+    tarjeta_inicio = int(sys.argv[1])
+    delta = int(sys.argv[2])
+else:
+    tarjeta_inicio = 10000000
+    delta = 100
+
+tarjeta_fin = tarjeta_inicio + delta
+
 url = "http://www.gpssumo.com/movimientos/get_movimientos/"
+
+
+try:
+    with open("output.json", "r") as f:
+        tarjetas = json.load(f)
+except:
+    tarjetas = {}
+
 
 session = requests.session()
 
-tarjetas = {}
 for tarjeta in range(tarjeta_inicio, tarjeta_fin):
-    response = session.get(url + str(tarjeta))
-    tarjetas[tarjeta] = demjson.decode(response.text.strip())
+    if str(tarjeta) not in tarjetas:
+        response = session.get(url + str(tarjeta))
+        tarjetas[tarjeta] = demjson.decode(response.text.strip())
+
 
 with open("output.json", "w") as f:
     json.dump(tarjetas, f)
