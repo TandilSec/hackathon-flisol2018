dae04eb0dfaf270291000f4574f5b6a79e5cf811
2012lucho <lucho.2012.tandil@gmail.com>
2018-05-10 01:01:11 -0300

    Update dump-csv.py

--- dump-csv.py
+++ dump-csv.py
@@ -21,18 +21,24 @@ session = requests.session()
 
 with open('tarjetas.csv', 'wb') as myfile:
     out = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    encabezado = ["tarjeta", "hora", "transaccion", "tipo", "recorrido", "fecha", "saldo", "cant_pasajes", "boleto", "importe", "saldo_viajes", "linea", "unidad"]
+    out.writerow(encabezado)
 
     for tarjeta in range(tarjeta_inicio, tarjeta_fin):
         response = session.get(url + str(tarjeta))
         transacciones_tarjeta = demjson.decode(response.text.strip())
+
         try:
             iterator = iter(transacciones_tarjeta)
         except TypeError:
             print tarjeta,' no existente o error'
         else:
             print tarjeta
+
             for transaccion in transacciones_tarjeta:
                 row = [tarjeta]
+
                 for key, value in transaccion.iteritems():
                     row.append(value)
+
                 out.writerow(row)
