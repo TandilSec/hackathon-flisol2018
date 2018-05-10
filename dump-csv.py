75a46ab9c2630fb4b4c21ca9e0602fb3eb52874c
2012lucho <lucho.2012.tandil@gmail.com>
2018-05-10 00:50:36 -0300

    Update dump-csv.py

--- dump-csv.py
+++ dump-csv.py
@@ -17,25 +17,22 @@ tarjeta_fin = delta + tarjeta_inicio
 
 url = "http://www.gpssumo.com/movimientos/get_movimientos/"
 
-tarjetas = {}
-
 session = requests.session()
 
 with open('tarjetas.csv', 'wb') as myfile:
     out = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
     for tarjeta in range(tarjeta_inicio, tarjeta_fin):
-        if str(tarjeta) not in tarjetas:
-            response = session.get(url + str(tarjeta))
-            transacciones_tarjeta = demjson.decode(response.text.strip())
-            try:
-                iterator = iter(transacciones_tarjeta)
-            except TypeError:
-                print tarjeta,' no existente o error'
-            else:
-                print tarjeta
-                for transaccion in transacciones_tarjeta:
-                    row = [tarjeta]
-                    for key, value in transaccion.iteritems():
-                        row.append(value)
-                    out.writerow(row)
+        response = session.get(url + str(tarjeta))
+        transacciones_tarjeta = demjson.decode(response.text.strip())
+        try:
+            iterator = iter(transacciones_tarjeta)
+        except TypeError:
+            print tarjeta,' no existente o error'
+        else:
+            print tarjeta
+            for transaccion in transacciones_tarjeta:
+                row = [tarjeta]
+                for key, value in transaccion.iteritems():
+                    row.append(value)
+                out.writerow(row)
